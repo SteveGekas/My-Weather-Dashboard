dc1a522b640df22685057c9388db28600fc12ad9
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-02-02 21:56:32 -0500

    updated date and variables in JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,21 @@
-//API Key 50072d89ed0daa2ea39f9aaf102ace85
+var searchCity;
+var key = "&appid=50072d89ed0daa2ea39f9aaf102ace85";
+var units = "&units=imperial";
+var todayWeather = "https://api.openweathermap.org/data/2.5/weather?";
+var uvIndex = "https://api.openweathermap.org/data/2.5/uvi?lat=";
+var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var location = navigator.geolocation;
+var weatherIcon = "http://openweathermap.org/img/wn/";
+var searchcityArr = [];
+
+$(document).ready(function () {
+    //gets the date for now (whenever you open the app)
+    var todayDate = moment().format("mm Do YYYY");
+    console.log(todayDate);
+    $(".current-city").append(todayDate);
+
+
+
+
+
+})
